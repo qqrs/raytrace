96323a2028d797b81fdf6a8bbca7645734daf1a4
Russell Smith <qqrsmith@gmail.com>
2023-10-18 22:32:12 -0400

    Add matte materials

--- camera.h
+++ camera.h
@@ -13,6 +13,7 @@ class camera {
     double aspect_ratio = 1.0;
     int image_width = 100;
     int samples_per_pixel = 10;
+    int max_depth = 10;
 
     void render(const hittable& world) {
         initialize();
@@ -26,7 +27,7 @@ class camera {
                 color pixel_color(0, 0, 0);
                 for (int k = 0; k < samples_per_pixel; k++) {
                     ray r = get_ray(i, j);
-                    pixel_color += ray_color(r, world);
+                    pixel_color += ray_color(r, max_depth, world);
                 }
                 write_color(std::cout, pixel_color, samples_per_pixel);
             }
@@ -82,10 +83,15 @@ class camera {
         return (px * pixel_delta_u) + (py * pixel_delta_v);
     }
 
-    color ray_color(const ray& r, const hittable& world) const {
+    color ray_color(const ray& r, int depth, const hittable& world) const {
         hit_record rec;
-        if (world.hit(r, interval(0, infinity), rec)) {
-            return 0.5 * (rec.normal + color(1, 1, 1));
+
+        if (depth <= 0)
+            return color(0, 0, 0);
+
+        if (world.hit(r, interval(0.001, infinity), rec)) {
+            vec3 scatter_dir = random_on_hemisphere(rec.normal);
+            return 0.5 * ray_color(ray(rec.p, scatter_dir), depth-1, world);
         }
 
         auto dir = unit_vector(r.direction());
